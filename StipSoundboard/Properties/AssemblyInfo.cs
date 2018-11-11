2b7d96cade95ebedb1077e455b6ddf808480d971
Stan Wijckmans <stanw@aviovision.com>
2018-11-11 19:45:01 +0100

    Update version.

--- StipSoundboard/Properties/AssemblyInfo.cs
+++ StipSoundboard/Properties/AssemblyInfo.cs
@@ -13,6 +13,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("Release")]
 #endif
 
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
 [assembly: ComVisible(false)]
